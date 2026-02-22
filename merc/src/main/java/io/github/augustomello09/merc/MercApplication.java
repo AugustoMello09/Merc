bb520874eb18ec64a935265d4e1a14e3e4eec064
José Augusto Mello Crispim <joseaugusto.mello01@gmail.com>
2026-02-22 18:50:39 -0300

    project started

---
+++ merc/src/main/java/io/github/augustomello09/merc/MercApplication.java
@@ -0,0 +1,13 @@
+package io.github.augustomello09.merc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercApplication.class, args);
+	}
+
+}
---
+++ merc/src/test/java/io/github/augustomello09/merc/MercApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.augustomello09.merc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
